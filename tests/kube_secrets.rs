eeae9a069e92a262d7507b570ecb08da889e2643
David Muto (pseudomuto) <david.muto@gmail.com>
2024-12-31 12:34:37 -0500

    [kube-secrets]: Add Support for Namespaces
    Currently, running `rejson kube-secrets` will generate a secrets
    manifest for any dictionaries defined under the `kubernetes` key. This
    has been useful when using secrets for Helm charts for example. The CI
    server decrypts the manifest into the templates directory before running
    `helm upgrade`.
    
    However, there are times when we don't necessarily want the secrets to
    be created in the release namespace. For example, if we have a chart
    with subcharts, each deploying to their own respective namespaces.
    
    To make this scenario easier, I've added the option to specify a
    namespace per secret.
    
    ```json
    {
      "_public_key": " ... ",
      "kubernetes": {
        "my-secret": {
          "value": "EJ[1..."
        },
        "my-namespaced-secret": {
          "_namespace": "custom-ns",
          "value": "EJ[1..."
        }
      }
    }
    ```
    
    Running `rejson kube-secrets` on this file produces the following YAML.
    Note that only one of the secrets has a namespace defined.
    
    ```yaml
    apiVersion: v1
    kind: Secret
    metadata:
      name: my-secret
    data:
      value: BASE64_ENCODED_PLAINTEXT
    ---
    apiVersion: v1
    kind: Secret
    metadata:
      name: my-namespaced-secret
      namespace: custom-ns
    data:
      value: BASE64_ENCODED_PLAINTEXT
    ```

--- tests/kube_secrets.rs
+++ tests/kube_secrets.rs
@@ -1,6 +1,7 @@
+use std::fs;
+
 use anyhow::Result;
 use assert_cmd::Command;
-use std::fs;
 
 const PUB_KEY: &str = "b595226c62427adbfc4a809cd7577488a6d402b2f930e1d603164ae3191a616e";
 const PRIV_KEY: &str = "88649a9e83f8f1984ad35ac8e8e86529aab518572c0341f46d1e0bc97f676f2b";
@@ -21,6 +22,7 @@ fn kube_secrets() -> Result<()> {
                     "password": "EJ[1:t33Bwgtq7Zghz1P0D+8ZMiSypiQye4q9DWLuxaOrLEU=:MrpO2Q3ByLTTZCdDXhNwowZvRuVg7c63:lEwnFAwPtrNXb/IKwqXej9V8MjumSSP5Rg==]"
                 },
                 "database": {
+                    "_namespace": "testing",
                     "DATABASE_URL": "EJ[1:t33Bwgtq7Zghz1P0D+8ZMiSypiQye4q9DWLuxaOrLEU=:0w+6gl3gXIOQohjqZnmih8ZLWPVffurJ:7U6ZEcttjrkS5sA73T/y/hESIaoxJUA320XqBoFWvw==]"
                 }
             }
@@ -41,6 +43,7 @@ apiVersion: v1
 kind: Secret
 metadata:
   name: database
+  namespace: testing
 data:
   DATABASE_URL: cGdzcWw6Ly9zb21lLWRi"#;
 
